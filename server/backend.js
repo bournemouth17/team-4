532661770eac2fd1a0c8ca131b92fb1d843f729c
Daniel Marshall <daniel.j.m-t@hotmail.com>
2017-06-10 00:34:30 +0100

    Started RESTful api

--- server/backend.js
+++ server/backend.js
@@ -9,36 +9,45 @@ var dbURL = "https://3656a298-714c-4c80-8e15-51f09b165d47-bluemix:a9ae2046ce73e2
 var connectionStatus = false;
 //This is making the connection to desired Cloudant instance using a cloudant module function "Cloudant()" with a callback containing an error code or the connection saved to a variable.
 var connection = Cloudant(dbURL, function(error, connection) {
-  if (!error) {
-    connectionStatus = true;
-    console.log("connection successful");
-  } else {
-    console.log(error.message);
-  }
+	if (!error) {
+		connectionStatus = true;
+		console.log("connection successful");
+	} else {
+		console.log(error.message);
+	}
 });
 //Selecting the database to use through the previous callbacks connection variable and storing it.
 var databaseToQuery = connection.db.use("questions");
 
+var allowCrossDomain = function(req, res, next) {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+	res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+	next();
+}
+
+app.use(allowCrossDomain);
+
 app.get("/questions", function(req, res) {
-  databaseToQuery.find({
-      "selector": {
-        "id": {
-          "$gt": 0
-        }
-      }
-    },
-    function(error, data) {
-      if (error) return console.log(error);
-      //sending the result back as the response in json format.
-      console.log(data.docs);
-      res.send(data.docs);
-      res.sendStatus(200);
-      myCache.set("questions", data.docs, function(error, success) {
-        if (success) {
-          console.log("questions been cached");
-        }
-      })
-    });
+	databaseToQuery.find({
+			"selector": {
+				"id": {
+					"$gt": 0
+				}
+			}
+		},
+		function(error, data) {
+			if (error) return console.log(error);
+			//sending the result back as the response in json format.
+			console.log(data.docs);
+			res.send(data.docs);
+			myCache.set("questions", data.docs, function(error, success) {
+				if (success) {
+					console.log("questions been cached");
+				}
+			})
+		});
 });
 
 app.listen(3000);
