0e387a556011a2f6cad5932dbdb1d5005726e299
Daniel Marshall <daniel.j.m-t@hotmail.com>
2017-06-10 07:13:50 +0100

    Documentation #BestCodeEver

--- utils.js
+++ utils.js
@@ -2,6 +2,10 @@ var apiUrl = "http://localhost:3000";
 var score = 0;
 var questions = {};
 var menuItems = [];
+
+/**
+ * POSTs the user input to persist it in the database
+ */
 var rnliForms = function() {
     var issue = $("#Issue").val();
     var location = $("#Location").val();
@@ -9,23 +13,32 @@ var rnliForms = function() {
     var responseTime = $("#ResponseTime").val();
     var date = $("#Date").val();
     var path = "/rnli/" + date + "/" + responseTime + "/" + location + "/" + issue + "/" + comment;
+
     doRequest(apiUrl + path, "POST");
+
 };
-var renderQuestion = function(options) {
+
+/**
+ * Renders the UI for the given question
+ * @param {Object} question - The question object returned from the cloudant db
+ */
+var renderQuestion = function(question) {
     $.Mustache.load('./templates.mustache')
         .done(function() {
-            $('#content').html($.Mustache.render('question', options));
-            $('#responses').html($.Mustache.render('responses', options));
+            $('#content').html($.Mustache.render('question', question));
+            $('#responses').html($.Mustache.render('responses', question));
             $('#responses div').each(function() {
                 if ($(this).attr("score") != "0") {
+                    //If the response has a non-zero score, highlight it as red for colour coding of fields
+                    //This should make it easier to identify
                     $(this).find("a").addClass("btn-danger");
                 } else {
                     $(this).find("a").addClass("btn-success");
                 }
-                $(this).click(function() {
+                $(this).click(function() { //attach click events to the rendered buttons
                     score += $(this).attr("score");
                     var nextID = $(this).attr("nextid");
-                    if (nextID == "EVAL") {
+                    if (nextID == "EVAL") { //If we have reached the end of the evaluation, render the result
                         doEvaluation();
                     } else if (nextID == "PATIENT_FORM") {
                         renderPatientForm();
@@ -37,32 +50,39 @@ var renderQuestion = function(options) {
         });
 };
 
+/**
+ * Renders the UI for the for the patient data input form
+ */
 var renderPatientForm = function() {
-
     $.Mustache.load('./templates.mustache')
         .done(function() {
-              $('#content').html($.Mustache.render('patientData'));
-              $('#responses').html($.Mustache.render('patientDataSubmit'));
+            $('#content').html($.Mustache.render('patientData'));
+            $('#responses').html($.Mustache.render('patientDataSubmit'));
         });
 };
 
+/**
+ * Renders the menu to access category specific cards
+ * @param {string} categoryID - The id of the category to render, if it has been selected. If null, categories will be rendered.
+ */
 var renderMenu = function(categoryID) {
     $.Mustache.load('./templates.mustache')
         .done(function() {
             var options = {};
-            if (categoryID) {
+            if (categoryID) { //If a category has been specified, get the sub-options to render
                 for (var i in menuItems) {
                     var item = menuItems[i];
                     if (item.id == categoryID) {
                         options["menuItems"] = item.options;
                     }
                 }
-            } else {
+            } else { //otherwise menu
                 options["menuItems"] = menuItems;
             }
 
             $('#content').html($.Mustache.render('contents', options));
             $('#responses').html("");
+            
             if (categoryID) {
                 $('#content li').each(function() {
                     $(this).click(function() {
@@ -81,6 +101,9 @@ var renderMenu = function(categoryID) {
         });
 };
 
+/**
+ * Renders the result of the bick sick/little sick evaluation
+ */
 var doEvaluation = function() {
     $.Mustache.load('./templates.mustache')
         .done(function() {
@@ -102,11 +125,15 @@ var doEvaluation = function() {
                     renderMenu();
                 });
             });
-            //$('.container').mustache('responses', options);
         });
 };
 
-
+/**
+ * Renders the menu to access category specific cards
+ * @param {string} targetUrl - target url for the http request
+ * @param {string} method - method for the http request, EG POST, GET, PATCH
+ * @param {function} callback - callback function to be called when the async request has completed
+ */
 var doRequest = function(targetUrl, method, callback) {
     var xmlReq = new XMLHttpRequest();
     xmlReq.onreadystatechange = function() {
@@ -118,6 +145,10 @@ var doRequest = function(targetUrl, method, callback) {
     xmlReq.send(null);
 };
 
+/**
+ * Processes the questions response from the database
+ * @param {string} response - JSON string of the questions queried from the DB
+ */
 var processQuestions = function(response) {
     response = JSON.parse(response);
     for (var r in response) {
@@ -127,6 +158,10 @@ var processQuestions = function(response) {
     renderQuestion(questions["1"]);
 };
 
+/**
+ * Processes the menu item response from the database
+ * @param {string} response - JSON string of the menu items queried from the DB
+ */
 var processMenu = function(response) {
     response = JSON.parse(response);
     for (var r in response[0].contents) {
